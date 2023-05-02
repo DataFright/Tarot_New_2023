c9d27e2ea9087745f47fd3fe7a120ae464653551
Matthew Swaney <Matthew.J.Swaney@gmail.com>
2023-05-02 14:49:28 -0500

    update

--- main.py
+++ main.py
@@ -25,117 +25,42 @@ question = input("What question will you ask the cards? ")
 print("\n")
 
 
-def past():
-    random_card_past = random.choice(td_keys)
-    random_int_1 = random.randint(0, 1)
+def get_reading(reading_type):
+    random_card = random.choice(td_keys)
+    random_int = random.randint(0, 1)
 
-    print("you asked the cards: ", question, ", and the cards said this about your past...\n")
+    print(f"you asked the cards: {question}, and the cards said this about your {reading_type}...\n")
 
-    if random_int_1 == 1:
-        print(td.tarot_deck[random_card_past]["name"], "\n", td.tarot_deck[random_card_past]["description"], "\n",
-              td.tarot_deck[random_card_past]["normal reading past"])
+    if random_int == 1:
+        print(td.tarot_deck[random_card]["name"], "\n", td.tarot_deck[random_card]["description"], "\n",
+              td.tarot_deck[random_card][f"normal {reading_type}"])
     else:
-        print(td.tarot_deck[random_card_past]["name"], "reversed", "\n", td.tarot_deck[random_card_past]["description"],
-              "\n",
-              td.tarot_deck[random_card_past]["upsidedown reading past"])
+        print(td.tarot_deck[random_card]["name"], "reversed", "\n", td.tarot_deck[random_card]["description"], "\n",
+              td.tarot_deck[random_card][f"upsidedown {reading_type}"])
 
-    td_keys.remove(random_card_past)
+    td_keys.remove(random_card)
 
     print("\n")
 
 
-def present():
-    random_card_present = random.choice(td_keys)
-    random_int_2 = random.randint(0, 1)
+def theDraw(how_many):
+    reading_types = {
+        "1": "past",
+        "2": "present",
+        "3": "future",
+        "4": "career",
+        "5": "love",
+    }
 
-    print("you asked the cards: ", question, ", and the cards said this about your present...\n")
-
-    if random_int_2 == 1:
-        print(td.tarot_deck[random_card_present]["name"], "\n", td.tarot_deck[random_card_present]["description"], "\n",
-              td.tarot_deck[random_card_present]["normal present"])
-    else:
-        print(td.tarot_deck[random_card_present]["name"], "reversed", "\n",
-              td.tarot_deck[random_card_present]["description"], "\n",
-              td.tarot_deck[random_card_present]["upsidedown present"])
-
-    td_keys.remove(random_card_present)
-
-    print("\n")
-
-
-def future():
-    random_card_future = random.choice(td_keys)
-    random_int_3 = random.randint(0, 1)
-
-    print("you asked the cards: ", question, ", and the cards said this about your future...\n")
-
-    if random_int_3 == 1:
-        print(td.tarot_deck[random_card_future]["name"], "\n", td.tarot_deck[random_card_future]["description"], "\n",
-              td.tarot_deck[random_card_future]["normal future"])
-    else:
-        print(td.tarot_deck[random_card_future]["name"], "reversed", "\n",
-              td.tarot_deck[random_card_future]["description"], "\n",
-              td.tarot_deck[random_card_future]["upsidedown future"])
-
-    td_keys.remove(random_card_future)
-
-    print("\n")
-
-
-def career():
-    random_card_career = random.choice(td_keys)
-    random_int_4 = random.randint(0, 1)
-
-    print("you asked the cards: ", question, ", and the cards said this about your career...\n")
-
-    if random_int_4 == 1:
-        print(td.tarot_deck[random_card_career]["name"], "\n", td.tarot_deck[random_card_career]["description"], "\n",
-              td.tarot_deck[random_card_career]["normal career"])
-    else:
-        print(td.tarot_deck[random_card_career]["name"], "reversed", "\n",
-              td.tarot_deck[random_card_career]["description"], "\n",
-              td.tarot_deck[random_card_career]["upsidedown career"])
-
-    td_keys.remove(random_card_career)
-
-    print("\n")
-
-
-def love():
-    random_card_love = random.choice(td_keys)
-    random_int_5 = random.randint(0, 1)
-
-    print("you asked the cards: ", question, ", and the cards said this about your love life...\n")
-
-    if random_int_5 == 1:
-        print(td.tarot_deck[random_card_love]["name"], "\n", td.tarot_deck[random_card_love]["description"], "\n",
-              td.tarot_deck[random_card_love]["normal love"])
-    else:
-        print(td.tarot_deck[random_card_love]["name"], "reversed", "\n", td.tarot_deck[random_card_love]["description"],
-              "\n",
-              td.tarot_deck[random_card_love]["upsidedown love"])
-
-    td_keys.remove(random_card_love)
-
-    print("\n")
-
-def theDraw():
     for i in how_many:
         for h in i:
-            if h == "1":
-                past()
-            elif h == "2":
-                present()
-            elif h == "3":
-                future()
-            elif h == "4":
-                career()
-            elif h == "5":
-                love()
+            if h in reading_types:
+                get_reading(reading_types[h])
             else:
-                print("invalid input, please try again")
+                print(f"\n{h} is an invalid input, please try again\n")
 
-theDraw()
+
+theDraw(how_many)
 
 print("Go with peace for the foreknowledge you have gained may change your path for the better \n"
       "or the worse, but you will be prepared for what is to come")
